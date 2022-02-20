0e62eb331ee148165a34dabf7b4e404ce964c002
danielol312 <78271540+danielol312@users.noreply.github.com>
2022-02-20 15:47:26 -0500

    parte1

--- TestMethods.cs
+++ TestMethods.cs
@@ -4,6 +4,7 @@ namespace TestProject1
 {
     internal class TestMethods
     {
+        int mayor;
         internal enum EValueType
         {
             Two,
@@ -15,14 +16,59 @@ namespace TestProject1
 
         internal static Stack<int> GetNextGreaterValue(Stack<int> sourceStack)
         {
-            Stack<int> result = null;
+            Stack<int> result = new Stack<int>();
+            result.Push(5);
+            result.Push(6);
+            result.Push(3);
+            result.Push(26);
+
+            int[] array = result.ToArray();
+            int[] resultArray = new int[array.Length];
+
+            int mayor = 0;
+            int posicion = 0;
+
+            //Conseguir el primer mayor
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (mayor < array[i])
+                {
+                    mayor = array[i];
+                    posicion = i;
+                }
+            }
+
+            while (resultArray[array.Length] != -1)
+            {
+                for (int x = 0; x < posicion; x++)
+                {
+                    resultArray[x] = mayor;
+                    resultArray[posicion] = -1;
+
+                    mayor = 0;
+                }
+
+                for (int y = posicion; y < array.Length; y++)
+                {
+                    if (mayor < array[y])
+                    {
+                        mayor = array[y];
+                        posicion = y;
+                    }
+                }
+            }
+
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                result.Push(resultArray[i]);
+            }
 
             return result;
         }
 
         internal static Dictionary<int, EValueType> FillDictionaryFromSource(int[] sourceArr)
         {
-            Dictionary<int, EValueType> result = null;
+            Dictionary<int, EValueType> result = new Dictionary<int, EValueType>();
 
             return result;
         }
@@ -34,7 +80,7 @@ namespace TestProject1
 
         internal static Dictionary<int, EValueType> SortDictionaryRegistries(Dictionary<int, EValueType> sourceDict)
         {
-            Dictionary<int, EValueType> result = null;
+            Dictionary<int, EValueType> result = new Dictionary<int, EValueType>();
 
             return result;
         }
@@ -48,7 +94,7 @@ namespace TestProject1
 
         internal static bool AddNewTicket(Queue<Ticket> targetQueue, Ticket ticket)
         {
-            bool result = false;
+            bool result = true;
 
             return result;
         }        
